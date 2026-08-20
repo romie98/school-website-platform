86febaee02edc6835bae1c603d99ff0a158640ae
ROMIE98 <romariocohen98@gmail.com>
2026-08-20 14:45:32 -0500

    Fix Alembic revision ID length for PostgreSQL

--- backend/alembic/versions/0003_add_nullable_school_id_tables.py
+++ backend/alembic/versions/0003_add_nullable_school_id_tables.py
@@ -3,7 +3,7 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-revision: str = "0003_add_nullable_school_id_tables"
+revision: str = "0003_school_nullable"
 down_revision: Union[str, None] = "0002_insert_default_school"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
--- backend/alembic/versions/0004_assign_default_school.py
+++ backend/alembic/versions/0004_assign_default_school.py
@@ -3,7 +3,7 @@ from typing import Sequence, Union
 from alembic import op
 
 revision: str = "0004_assign_default_school"
-down_revision: Union[str, None] = "0003_add_nullable_school_id_tables"
+down_revision: Union[str, None] = "0003_school_nullable"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
